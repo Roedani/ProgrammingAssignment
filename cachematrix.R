4d939604ba372f2ef52e24fad429f9d240a10896
Roedani <74432483+Roedani@users.noreply.github.com>
2021-03-02 12:32:34 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inver <- NULL 
+    set <- function(y) {  
+        x <<- y # here will create an object in the global environment
+        inver <<- NULL
+    }
+    get_matrix <- function() x
+    set_inver <- function(solmatrix) inver <<- solmatrix
+    get_inver <- function() inver
+    list(set = set, get_matrix = get_matrix,
+         set_inver = set_inver,
+         get_inver = get_inver)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { # this function will use the makeCacheMatrix as output
+        inver <- x$get_inver()
+if(!is.null(inver)) {
+        message("getting cached data")
+        return(inver)
 }
+        ## Return a matrix that is the inverse of 'x'
+        data <- x$get_matrix()
+        inver <- solve(data)
+        x$set_inver(inver)
+        inver
+        
+}
+
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+myMatrix_object <- makeCacheMatrix(m1)
+
+n1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
+myMatrix_object <- makeCacheMatrix(n1)
+
+cacheSolve(myMatrix_object)
